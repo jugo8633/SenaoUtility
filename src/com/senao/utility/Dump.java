30c01992bb9d8a6c1a82687af2a82df2abdf1fd4
jugo8633 <jugo.tw@gmail.com>
2015-05-15 08:42:31 +0800

    add

---
+++ src/com/senao/utility/Dump.java
@@ -0,0 +1,13 @@
+package com.senao.utility;
+
+public class Dump
+{
+	public String strCallUrl = null;
+	public String strException = null;
+	public String strResponse = null;
+	
+	public Dump()
+	{
+		
+	}
+}
